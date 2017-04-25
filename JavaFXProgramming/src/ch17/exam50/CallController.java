6b98712b6a4b8ed032d6aef9b98dee89f8544c9c
Jangyukyung <jyk602222@naver.com>
2017-04-25 18:02:34 +0900

    4.25

---
+++ JavaFXProgramming/src/ch17/exam50/CallController.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch17.exam50;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+/**
+ * FXML Controller class
+ *
+ * @author Administrator
+ */
+public class CallController implements Initializable {
+
+	@FXML
+	private TextArea textArea;
+	@FXML
+	private Button btn4;
+	@FXML
+	private Button btn2;
+	@FXML
+	private Button btn3;
+	@FXML
+	private Button btn6;
+	@FXML
+	private Button btn5;
+	@FXML
+	private Button btn1;
+	@FXML
+	private Button btn9;
+	@FXML
+	private Button btn8;
+	@FXML
+	private Button btn7;
+	@FXML
+	private Button btn0;
+	@FXML
+	private Button btnCall;
+	@FXML
+	private Button btnCancel;
+
+	/**
+	 * Initializes the controller class.
+	 */
+	@Override	
+	 public void initialize(URL location, ResourceBundle resources) {
+       // System.out.println(btn1.getText());
+		
+        btn1.setOnAction(event ->{
+             System.out.println("버튼1");
+        });
+		
+        btn2.setOnAction(event ->{
+             System.out.println("버튼2 클릭됨");
+        });
+        
+       btn3.setOnAction(event ->{
+             System.out.println("버튼3 클릭됨");
+        });
+        
+	 btn4.setOnAction(event ->{
+             System.out.println("버튼4 클릭됨");
+        });
+	 
+        btn5.setOnAction(event ->{
+            System.out.println("버튼 5 ");
+        });
+		
+    }
+
+    
+	 
+
+}
