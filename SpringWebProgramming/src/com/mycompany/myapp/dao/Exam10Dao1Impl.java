ab4e9d70ffc2a2ad98c38f9fb20a94d01efe8292
Jangyukyung <jyk602222@naver.com>
2017-05-23 17:50:41 +0900

    5.23
    Web - di / log
    Raspberry - ADC/DAC

--- SpringWebProgramming/src/com/mycompany/myapp/dao/Exam10Dao1Impl.java
+++ SpringWebProgramming/src/com/mycompany/myapp/dao/Exam10Dao1Impl.java
@@ -2,15 +2,15 @@ package com.mycompany.myapp.dao;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Exam10DaoImpl implements Exam10Dao{
+public class Exam10Dao1Impl implements Exam10Dao1{
 	
 	@Override
 	public void insert(){
-		System.out.println("insert() 실행 ! ");
+		System.out.println("Exam10Dao1Impl insert() 실행 ! ");
 	}
 	
 	@Override
 	public void select(){
-		System.out.println("select() 실행 ! ");
+		System.out.println("Exam10Dao1Impl select() 실행 ! ");
 	}
 }
--- SpringWebProgramming/src/com/mycompany/myapp/service/Exam10Service1Impl.java
+++ SpringWebProgramming/src/com/mycompany/myapp/service/Exam10Service1Impl.java
@@ -1,23 +1,25 @@
 package com.mycompany.myapp.service;
 
-import com.mycompany.myapp.dao.Exam10Dao;
+import com.mycompany.myapp.dao.Exam10Dao1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Exam10ServiceImpl implements Exam10Service{
+public class Exam10Service1Impl implements Exam10Service1{
+	
+	//필드주입
 	@Autowired
-	private Exam10Dao exam10Dao;
+	private Exam10Dao1 exam10Dao;
 	
 	@Override
 	public void join(){
-		System.out.println("join() 실행 ! ");
+		System.out.println("Exam10Service1Impl join() 실행 ! ");
 		exam10Dao.insert();
 	}
 	
 	@Override
 	public void login(){
-		System.out.println("login() 실행 ! ");
+		System.out.println("Exam10Service1Impl login() 실행 ! ");
 		exam10Dao.select();
 	}
 }
