b848b985cd6423fec3c8b650f33543b6953db6f9
Jangyukyung <jyk602222@naver.com>
2017-06-15 17:57:39 +0900

    6.15
    raspberry- CoAP 프록시를 이용한 HTTP 통신

---
+++ IoTWebProgramming/src/test/java/com/mycompany/myapp/service/ServiceTest.java
@@ -0,0 +1,19 @@
+package com.mycompany.myapp.service;
+
+import org.junit.Test;
+
+import com.mycompany.myapp.ApplicationContextLoader;
+
+public class ServiceTest extends ApplicationContextLoader{
+	
+	@Test
+	public void writeTest(){
+		//테스트코드
+	}
+	
+	@Test
+	public void getTest(){
+		//테스트코드
+	}
+	
+}
